54123da5f6d941537c6d5798ee87f7e9677323e8
Tolga Demiralp <tolgademiralp@outlook.com>
2022-07-05 16:06:16 +0300

    Brand ve User nesneleri için validation kuralları yazıldı.

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Email).Must(EmailIsValid).WithMessage("Email biçimi doğru değil.");
+
+        }
+
+        public bool EmailIsValid(string email)
+        {
+            string expression = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+
+            if (Regex.IsMatch(email, expression))
+            {
+                if (Regex.Replace(email, expression, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
