82a8d1678de16cfbb53aab41728a6a4da34217f9
Tolga Demiralp <tolgademiralp@outlook.com>
2022-07-05 15:34:35 +0300

    AOP, FluentValidation ve Autofac desteği projeye eklendi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities;
 using Entities.Concrete;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,13 +23,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.Description.Length < 2 && car.DailyPrice < 0)
-            {
-                return new ErrorResult(Messages.ItemNameInvaild);
-            }
-
             _carDal.Add(car);
             return new SuccessResult(Messages.ItemAdded);
         }
--- WebAPI/Controllers/CustomersController.cs
+++ WebAPI/Controllers/CustomersController.cs
@@ -30,6 +30,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpGet("getbyid")]
         public IActionResult GetById(int id)
         {
